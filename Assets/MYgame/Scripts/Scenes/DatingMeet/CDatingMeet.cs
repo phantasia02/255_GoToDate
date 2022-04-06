3f60a98008f2ec03d4b36df2404eb3ddff3b690e
phantasia02 <https://github.com/phantasia02/UsavichSolitaireForTaiwan>
2022-04-06 14:24:25 +0800

    update DatingMeet

--- Assets/MYgame/Scripts/Scenes/DatingMeet/CDatingMeet.cs
+++ Assets/MYgame/Scripts/Scenes/DatingMeet/CDatingMeet.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+using UnityEngine.Events;
+
 
 public class CDatingMeet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] protected GameObject m_WinTimeline = null;
+    [SerializeField] protected GameObject m_OverTimeline = null;
+    [SerializeField] protected SignalReceiver m_MyEndEvent = null;
+
+    private void Awake()
     {
         
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void EndFunc()
     {
-        
+        Debug.Log("EndFunc OK");
     }
+
 }
