77e240429650fbc78bec88064fee77ad5d6484ce
phantasia02 <https://github.com/phantasia02/UsavichSolitaireForTaiwan>
2022-04-18 18:02:03 +0800

    update Small Game 001

--- Assets/MYgame/Scripts/Scenes/S001SmallGame/CAll3DLineCtrl.cs
+++ Assets/MYgame/Scripts/Scenes/S001SmallGame/CAll3DLineCtrl.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using UniRx;
 using UniRx.Triggers;
 using DG.Tweening;
+using Dreamteck.Splines;
 
 public class CAll3DLineCtrl : MonoBehaviour
 {
@@ -30,15 +31,17 @@ public class CAll3DLineCtrl : MonoBehaviour
 
     // ==================== SerializeField ===========================================
 
-    [SerializeField] protected Material         m_2DLineMat = null;
     [SerializeField] protected GameObject       m_UIParent  = null;
     [SerializeField] protected float            m_MaxBlendVal = 1.0f;
     [SerializeField] protected float            m_AddLoveRefVal = 1.0f;
     [SerializeField] protected float            m_MaxLoveVal = 100.0f;
+    [SerializeField] protected SplineFollower m_CurRatioLoveVal = null;
+    [SerializeField] protected GameObject       m_ShowLineCollider = null;
+
+    
 
     // ==================== SerializeField ===========================================
 
-    protected LineRenderer     m_3DLine    = null;
     protected CCamGLDraw m_CamGLDraw = null;
     protected List<DataPointInfo> m_All2DDataPointInfo = new List<DataPointInfo>();
     protected DataPointInfo m_CurPointData = null;
@@ -59,7 +62,6 @@ public class CAll3DLineCtrl : MonoBehaviour
 
     private void Awake()
     {
-        m_3DLine = this.GetComponentInChildren<LineRenderer>();
 
         var lTempUpdateAs = this.UpdateAsObservable();
     
@@ -101,53 +103,9 @@ public class CAll3DLineCtrl : MonoBehaviour
 
     private void Start()
     {
-        CGGameSceneData lTempGameSceneData = CGGameSceneData.SharedInstance;
-
-        Vector3[] lTempArrPos = new Vector3[20];
-        Vector3 lTempHitPos = Vector3.zero;
-        int lTempposCount  = m_3DLine.GetPositions(lTempArrPos);
-
-        DataPointInfo lTempCurDataPoint   = null;
-        DataPointInfo lTempPreviousDataPoint = null;
-        m_TotalDis = 0.0f;
-
-        for (int i = 0; i < lTempposCount; i++)
-        {
-            lTempHitPos = Camera.main.WorldToScreenPoint(lTempArrPos[i]);
-
-            //GameObject lTempHitUI = null;
-            //lTempHitUI = GameObject.Instantiate(lTempGameSceneData.m_AllOtherObj[(int)CGGameSceneData.EOtherObj.eHitUIObj], m_UIParent.transform);
-            //lTempHitUI.transform.position = lTempHitPos;
-
-            lTempCurDataPoint = new DataPointInfo();
-          
-            lTempCurDataPoint.m_2DPoint.x = lTempHitPos.x;
-            lTempCurDataPoint.m_2DPoint.y = lTempHitPos.y;
-            lTempCurDataPoint.m_ScreenPoint.x = lTempHitPos.x / (float)Screen.width;
-            lTempCurDataPoint.m_ScreenPoint.y = lTempHitPos.y / (float)Screen.height;
-            lTempCurDataPoint.m_2DPoint.z = lTempHitPos.z = 0.0f;
-            
-            // Debug.Log($"lTempCurDataPoint.m_2DPoin = {lTempCurDataPoint.m_2DPoint}");
-            
-            m_All2DDataPointInfo.Add(lTempCurDataPoint);
-
-            if (i != 0)
-            {
-                lTempPreviousDataPoint.m_2DDir = lTempCurDataPoint.m_2DPoint - lTempPreviousDataPoint.m_2DPoint;
-                lTempPreviousDataPoint.m_Dis = (lTempCurDataPoint.m_2DPoint - lTempPreviousDataPoint.m_2DPoint).magnitude;
-                m_TotalDis += lTempPreviousDataPoint.m_Dis;
-               // Debug.Log($"lTempPreviousDataPoint.m_Dis = {lTempPreviousDataPoint.m_Dis}");
-                lTempPreviousDataPoint.m_2DDir.z = 0.0f;
-                lTempPreviousDataPoint.m_2DDir.Normalize();
-                lTempCurDataPoint.m_CurAccumulationDis = m_TotalDis;
-            }
-            //Debug.Log($"m_TotalDis = {m_TotalDis}");
-            //Debug.Log($"m_All2DDataPointInfo[{i}] = {m_All2DDataPointInfo[i].m_CurAccumulationDis}");
-            lTempPreviousDataPoint = lTempCurDataPoint;
-        }
-       // Debug.Log($"m_TotalDis = {m_TotalDis}");
+      
         m_OBE3DLineCtrlState.Value = E3DLineCtrlState.eReady;
-        m_All2DDataPointInfo[m_All2DDataPointInfo.Count - 1].m_CurAccumulationDis = m_TotalDis;
+       // m_All2DDataPointInfo[m_All2DDataPointInfo.Count - 1].m_CurAccumulationDis = m_TotalDis;
     }
 
     public void Update()
@@ -165,20 +123,6 @@ public class CAll3DLineCtrl : MonoBehaviour
     //    Debug.Log("OnCollisionEnter");
     //}
 
-    public void UpdateNextCurPointData()
-    {
-        //if (m_NextPointData != null)
-        //{
-        //    Debug.Log("Win!!!!!!!!!!");
-        //}
-
-        //m_CurDis += m_NextPointData.m_Dis;
-        m_TargetDis += m_NextPointData.m_Dis;
-        m_CurIndex = m_CurIndex + 1;
-        m_CurPointData = m_NextPointData;
-        UpdateNextPointData();
-    }
-
     public void UpdateNextPointData()
     {
         if (m_CurIndex + 1 >= m_All2DDataPointInfo.Count)
@@ -193,84 +137,53 @@ public class CAll3DLineCtrl : MonoBehaviour
 
     public void OnMouseDown()
     {
+
+
         if (m_OBE3DLineCtrlState.Value != E3DLineCtrlState.eReady)
             return;
 
         //Debug.Log("OnMouseDown");
 
-        if (m_All2DDataPointInfo.Count <= m_CurIndex)
-            return;
+        //if (m_All2DDataPointInfo.Count <= m_CurIndex)
+        //    return;
         
         m_OBE3DLineCtrlState.Value = E3DLineCtrlState.eMouseDrag;
-        m_CurPointData = m_All2DDataPointInfo[m_CurIndex];
-        UpdateNextPointData();
 
-        if (m_CamGLDraw != null)
-        {
-            m_CamGLDraw.startVertex = m_CurPointData.m_ScreenPoint;
-            m_CamGLDraw.OpenDrewGL = true;
-        }
+
+        m_ShowLineCollider.gameObject.SetActive(true);
+
+        //m_CurPointData = m_All2DDataPointInfo[m_CurIndex];
+        //UpdateNextPointData();
+
+        //if (m_CamGLDraw != null)
+        //{
+        //    m_CamGLDraw.startVertex = m_CurPointData.m_ScreenPoint;
+        //    m_CamGLDraw.OpenDrewGL = true;
+        //}
+
+        SplineSample lTempSplineSample = m_CurRatioLoveVal.spline.Project(this.transform.position);
+        m_CurRatioLoveVal.SetPercent(0.0f);
     }
 
+
+   
 
     public void OnMouseDrag()
     {
         if (OBE3DLineCtrlStateVal().Value == E3DLineCtrlState.eMouseDrag)
         {
-            if (m_CurPointData == null)
-                return;
 
-            Vector3 lInputMousePoint = Input.mousePosition;
-            Vector3 lScreenInputMousePos = Vector3.zero;
-            Vector3 lScreenInputMouseDir = Input.mousePosition;
-            //lScreenInputMouseNormal.x = lScreenInputMouseNormal.x / Screen.width;
-            //lScreenInputMouseNormal.y = lScreenInputMouseNormal.y / Screen.height;
-            lScreenInputMousePos = lScreenInputMouseDir;
-            lScreenInputMouseDir = lScreenInputMouseDir - m_CurPointData.m_2DPoint;
-            
-            lScreenInputMousePos.z = lInputMousePoint.z = lScreenInputMouseDir.z = 0.0f;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-
-            Vector3 lTempCross = Vector3.Cross(m_CurPointData.m_2DDir, lScreenInputMouseDir);
-
-            //Debug.Log($"========================");
-            //Debug.Log($"lTempCross = {Mathf.Abs(lTempCross.z)}");
-            //Debug.Log($"m_CurPointData.m_2DDir = {m_CurPointData.m_2DDir}");
-            //Debug.Log($"lScreenInputMouseNormal = {lScreenInputMouseDir}");
-
-            if (Mathf.Abs(lTempCross.z) >= m_CrossConfirm)
+            if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                m_OBE3DLineCtrlState.Value = E3DLineCtrlState.eReadyEnd;
-                return;
-            }
-            //float lTempDis = Vector3.Distance(lInputMousePoint, m_NextPointData.m_2DPoint);
+                SplineSample lTempSplineSample = m_CurRatioLoveVal.spline.Project(hit.point);
 
-            if ((lInputMousePoint - m_NextPointData.m_2DPoint).sqrMagnitude < m_NextOKRange)
-                UpdateNextCurPointData();
-
-            //Debug.Log($"m_CurPointData.m_CurAccumulationDis = {m_CurPointData.m_CurAccumulationDis}");
-            float lTempCurDis = m_CurPointData.m_CurAccumulationDis;
-
-            if (m_CurPointData != null)
-            {
-                float lTempMouseDisVal = Vector3.Distance(lInputMousePoint, m_CurPointData.m_2DPoint);
-                lTempMouseDisVal = Mathf.Min(m_CurPointData.m_Dis * 0.9f, lTempMouseDisVal);
-
-                float lTempDotVal = Vector3.Dot(m_CurPointData.m_2DDir, lScreenInputMouseDir.normalized);
-                if (lTempDotVal > 0.0f)
-                    lTempCurDis = m_CurPointData.m_CurAccumulationDis + lTempMouseDisVal;
-                else
-                    lTempCurDis = m_CurPointData.m_CurAccumulationDis - lTempMouseDisVal;
+                m_CurRatioLoveVal.SetPercent(lTempSplineSample.percent);
+                m_TargetDis = (float)lTempSplineSample.percent;
             }
             else
-                lTempCurDis = m_TotalDis;
-
-           
-            m_TargetDis = lTempCurDis;
-           
-
-            if (m_CamGLDraw != null && m_CurPointData != null)
-                m_CamGLDraw.startVertex = m_CurPointData.m_ScreenPoint;
+                m_OBE3DLineCtrlState.Value = E3DLineCtrlState.eReadyEnd;
         }
     }
 
@@ -278,8 +191,6 @@ public class CAll3DLineCtrl : MonoBehaviour
     {
         Debug.Log("OnMouseUp");
         m_OBE3DLineCtrlState.Value = E3DLineCtrlState.eReadyEnd;
-        if (m_CamGLDraw != null)
-            m_CamGLDraw.OpenDrewGL = false;
     }
 
     // ===================== UniRx ======================
